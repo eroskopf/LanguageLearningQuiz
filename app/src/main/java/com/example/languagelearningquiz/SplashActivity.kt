b10354bf33bb068bc417125ba65739b072a1685a
coroseth <rosethc@carleton.edu>
2022-11-28 17:18:24 +0100

    Updated Manifest and splash

--- app/src/main/java/com/example/languagelearningquiz/SplashActivity.kt
+++ app/src/main/java/com/example/languagelearningquiz/SplashActivity.kt
@@ -1,11 +1,51 @@
 package com.example.languagelearningquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.example.languagelearningquiz.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val splashAnim = AnimationUtils.loadAnimation(
+            this,
+            R.anim.logo_anim
+        )
+
+        splashAnim.setAnimationListener(
+
+            object : Animation.AnimationListener {
+                override fun onAnimationRepeat(p0: Animation?) {
+                }
+
+                override fun onAnimationStart(p0: Animation?) {
+                }
+
+                override fun onAnimationEnd(p0: Animation?) {
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                }
+            }
+        )
+        binding.logo.startAnimation(splashAnim)
+
+
+
+
+
+
+
+
+
+
+
     }
 }

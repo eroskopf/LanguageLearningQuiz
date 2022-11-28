06483929fbdc6fa9149b3ed4c5ad53e247de0a4d
eroskopf <roskopfe@carleton.edu>
2022-11-28 17:30:26 +0100

    working on ScoreActivity and made me install navgraph dependencies?

--- app/src/main/java/com/example/languagelearningquiz/ScoreActivity.kt
+++ app/src/main/java/com/example/languagelearningquiz/ScoreActivity.kt
@@ -2,10 +2,52 @@ package com.example.languagelearningquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.languagelearningquiz.databinding.ActivityMainBinding
+import com.example.languagelearningquiz.databinding.ActivityScoreBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class ScoreActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityScoreBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_score)
+        binding = ActivityScoreBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        auth = Firebase.auth
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        //updateUI(currentUser)
+        /*
+        customToken?.let {
+            auth.signInWithCustomToken(it)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithCustomToken:success")
+                        val user = auth.currentUser
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithCustomToken:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                        updateUI(null)
+                    }
+                }
+        }
+
+         */
     }
 }
